81ac88d4717d836c2c588b2e04c8f038aecd10c5
mohit-kandoi <mohitkandoi65@gmail.com>
2024-01-13 22:50:16 +0530

    added

--- src/LinkedList/FindMiddleOfLinkedList.java
+++ src/LinkedList/FindMiddleOfLinkedList.java
@@ -13,7 +13,7 @@ public class FindMiddleOfLinkedList {
         head.next = n1; n1.next = n2; n2.next = n3; n3.next = n4; n4.next = n5;
 
         Node<Integer> middle = FindMiddle(head);
-        System.out.println(middle.val);
+        System.out.print(middle.val);
 
     }
     public static Node<Integer> FindMiddle(Node<Integer> head){
