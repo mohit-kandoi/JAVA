518a0ea730ce6d8dcb5a80a7915af8662fc29fec
mohit-kandoi <mohitkandoi65@gmail.com>
2024-02-14 10:36:16 +0530

    Day 24

---
+++ src/BitManipulationAndMaths/ClearTheLastSetBit.java
@@ -0,0 +1,8 @@
+package BitManipulationAndMaths;
+
+public class ClearTheLastSetBit {
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println(n&n-1);
+    }
+}
---
+++ src/BitManipulationAndMaths/CountTheSetBit.java
@@ -0,0 +1,14 @@
+package BitManipulationAndMaths;
+
+public class CountTheSetBit {
+    public static void main(String[] args) {
+        int n = 14;
+
+        int count = 0;
+        while(n != 0){
+            if((n&1) == 1) count++;
+            n = n >> 1;
+        }
+        System.out.println(count);
+    }
+}
